a54faf92e5eee7f08ea9a03e14330c55c73fb97a
Sang Dao <daosysang@gmail.com>
2025-11-09 12:05:01 +0700

    Checked

--- init.lua
+++ init.lua
@@ -1,10 +1,26 @@
 require("config.lazy")
 
+vim.g.mapleader = ' '
+vim.opt.mouse = ''
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.expandtab = true
+vim.opt.wrap = false
+vim.opt.shiftround = true
+vim.opt.termguicolors = true
+vim.opt.ff = "unix"
+vim.opt.updatetime = 60000
+
 vim.o.background = "dark" -- or "light" for light mode
 vim.cmd([[colorscheme gruvbox]])
 
 vim.keymap.set("n", "<leader>d", "yyp", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>o", "o<Esc>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>O", "O<Esc>", { noremap = true, silent = true })
+
 vim.keymap.set("n", "<c-q>", ":qa<cr>", { noremap = true, silent = true })
+vim.keymap.set("n", "<S-k>", "k<S-j>", { noremap = true, silent = true })
 
 vim.keymap.set("n", "<F2>", ":wa<cr>", { noremap = true, silent = true })
 vim.keymap.set("i", "<F2>", "<Esc>:wa<cr>", { noremap = true, silent = true })
--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -25,26 +25,51 @@ vim.g.maplocalleader = "\\"
 require("lazy").setup({
   spec = {
     -- import your plugins
+
+	{"nvim-treesitter/nvim-treesitter", branch = 'master', lazy = false, build = ":TSUpdate"},
     { "ellisonleao/gruvbox.nvim", priority = 1000 , config = true, opts = ...},
     {
-  "nvim-tree/nvim-tree.lua",
-  version = "*",
-  lazy = false,
-  dependencies = {
-    "nvim-tree/nvim-web-devicons",
-  },
-  config = function()
-    require("nvim-tree").setup {}
-  end,
-},
-{
-  "ibhagwan/fzf-lua",
-  -- optional for icon support
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  -- or if using mini.icons/mini.nvim
-  -- dependencies = { "nvim-mini/mini.icons" },
-  opts = {}
-}
+	"nvim-tree/nvim-tree.lua",
+        version = "*",
+        lazy = false,
+        dependencies = {
+          "nvim-tree/nvim-web-devicons",
+        },
+	config = function()
+		require("nvim-tree").setup {}
+        end,
+    },
+    {
+	"ibhagwan/fzf-lua",
+	-- optional for icon support
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	-- or if using mini.icons/mini.nvim
+	-- dependencies = { "nvim-mini/mini.icons" },
+	opts = {
+		winopts = {
+			preview = {
+				hidden = true,
+			}
+		}
+	}
+    },
+    {
+	"kylechui/nvim-surround",
+	version = "^3.0.0", -- Use for stability; omit to use `main` branch for the latest features
+	event = "VeryLazy",
+	config = function()
+		require("nvim-surround").setup({
+		    -- Configuration here, or leave empty to use defaults
+		})
+	end
+    },
+    {
+       'windwp/nvim-autopairs',
+       event = "InsertEnter",
+       config = true
+       -- use opts = {} for passing setup options
+       -- this is equivalent to setup({}) function
+    }
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
