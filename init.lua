de6e6a028257724af599828b21c62f96bd7cf51a
Sang Dao <daosysang@gmail.com>
2025-11-09 16:12:43 +0700

    Checkpoint: awesome

--- init.lua
+++ init.lua
@@ -1,6 +1,8 @@
 require("config.lazy")
 
 vim.g.mapleader = ' '
+vim.g.python3_host_prog = '/usr/bin/python3'
+
 vim.opt.mouse = ''
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
@@ -11,6 +13,12 @@ vim.opt.shiftround = true
 vim.opt.termguicolors = true
 vim.opt.ff = "unix"
 vim.opt.updatetime = 60000
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.numberwidth = 3
+vim.opt.iskeyword:append("-")
+
+vim.opt.grepprg = "rg --ignore-file=.gitignore --vimgrep"
 
 vim.o.background = "dark" -- or "light" for light mode
 vim.cmd([[colorscheme gruvbox]])
@@ -18,15 +26,64 @@ vim.cmd([[colorscheme gruvbox]])
 vim.keymap.set("n", "<leader>d", "yyp", { noremap = true, silent = true })
 vim.keymap.set("n", "<leader>o", "o<Esc>", { noremap = true, silent = true })
 vim.keymap.set("n", "<leader>O", "O<Esc>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>j", "7j", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>k", "7k", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>h", "7h", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>l", "7l", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>n", ":noh<cr>", { noremap = true, silent = true })
 
 vim.keymap.set("n", "<c-q>", ":qa<cr>", { noremap = true, silent = true })
 vim.keymap.set("n", "<S-k>", "k<S-j>", { noremap = true, silent = true })
 
 vim.keymap.set("n", "<F2>", ":wa<cr>", { noremap = true, silent = true })
 vim.keymap.set("i", "<F2>", "<Esc>:wa<cr>", { noremap = true, silent = true })
-
 vim.keymap.set("n", "<F3>", ":NvimTreeToggle<cr>", { noremap = true, silent = true })
+
+vim.keymap.set("n", "gO", "<nop>", { noremap = true, silent = true })
+vim.keymap.set("n", "gO", ":FzfLua live_grep", { noremap = true, silent = true })
+vim.keymap.set("n", "go", "<nop>", { noremap = true, silent = true })
+vim.keymap.set("n", "go", ":FzfLua grep_quickfix", { noremap = true, silent = true })
+vim.keymap.set("n", "gn", ":cn <cr> :normal! zz <cr>", { noremap = true, silent = true })
+vim.keymap.set("n", "gp", ":cp <cr> :normal! zz <cr>", { noremap = true, silent = true })
 
 vim.keymap.set("n", "<c-j>", ":FzfLua files<cr>", { noremap = true, silent = true })
 vim.keymap.set("n", "<c-k>", ":FzfLua buffers<cr>", { noremap = true, silent = true })
 vim.keymap.set("n", "<c-n>", ":FzfLua blines<cr>", { noremap = true, silent = true })
+
+vim.api.nvim_create_user_command(
+  "Greeting", -- The name of your custom command
+  function()
+    print("Hello from MyCommand!")
+  end,
+  {
+    desc = "Simple custom comnand", -- Optional: Description for the command
+  }
+)
+
+local coq = require "coq" -- add this
+
+-- legacy style
+local lsp = require "lspconfig"
+-- lsp.pyright.setup(coq.lsp_ensure_capabilities()) -- after
+
+-- new style
+vim.lsp.config("pyright", coq.lsp_ensure_capabilities()) -- after
+vim.lsp.enable("pyright")
+
+
+local function hide_diagnostics()
+    vim.diagnostic.config({  -- https://neovim.io/doc/user/diagnostic.html
+        virtual_text = false,
+        signs = false,
+        underline = false,
+    })
+end
+local function show_diagnostics()
+    vim.diagnostic.config({
+        virtual_text = true,
+        signs = true,
+        underline = true,
+    })
+end
+vim.keymap.set("n", "<F7>", hide_diagnostics)
+vim.keymap.set("n", "<F8>", show_diagnostics)
--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -69,6 +69,35 @@ require("lazy").setup({
        config = true
        -- use opts = {} for passing setup options
        -- this is equivalent to setup({}) function
+    },
+    {
+        "neovim/nvim-lspconfig", -- REQUIRED: for native Neovim LSP integration
+        lazy = false, -- REQUIRED: tell lazy.nvim to start this plugin at startup
+        dependencies = {
+            -- main one
+            { "ms-jpq/coq_nvim", branch = "coq" },
+
+            -- 9000+ Snippets
+            { "ms-jpq/coq.artifacts", branch = "artifacts" },
+
+            -- lua & third party sources -- See https://github.com/ms-jpq/coq.thirdparty
+            -- Need to **configure separately**
+            { 'ms-jpq/coq.thirdparty', branch = "3p" }
+            -- - shell repl
+            -- - nvim lua api
+            -- - scientific calculator
+            -- - comment banner
+            -- - etc
+        },
+        init = function()
+            vim.g.coq_settings = {
+                auto_start = true, -- if you want to start COQ at startup
+                -- Your COQ settings here
+            }
+        end,
+        config = function()
+            -- Your LSP settings here
+        end,
     }
   },
   -- Configure any other settings here. See the documentation for more details.
